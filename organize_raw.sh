9abed35e5f8166bace64fec2cfce01e230f38b11
Kai Hwang <kai.hwang@gmail.com>
2018-04-20 14:16:07 -0700

    skip censor.

--- organize_raw.sh
+++ organize_raw.sh
@@ -2,9 +2,9 @@
 
 WD='/home/despoB/kaihwang/HTB_fMRI'
 
-for s in Sub41_Htb Sub42_Htb_Notms; do
+for s in sub43 sub44 sub45 sub46 sub48; do
 	
-	heudiconv -d ${WD}/Raw/{subject}/*/*/* -s ${s} \
+	heudiconv -d /home/despoC/HierarchyThetaBeta/fMRI_Experiment/Session2_Baseline/RawMRI/{subject}/*/*/* -s ${s} \
 	-f /home/despoB/kaihwang/bin/HTB_MRI/TTD_heuristics.py -c dcm2niix -o ${WD}/BIDS --bids
 
 done
